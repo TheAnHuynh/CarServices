237fe71e8c6ee5c29d3c3c115b947260743105d1
Huynh The An <flului1996@gmail.com>
2018-04-18 14:03:47 +0700

    2

---
+++ app/src/main/java/d14cqcp01/group5/carservices/DangKyTaiKhoanActivity.java
@@ -0,0 +1,16 @@
+package d14cqcp01.group5.carservices;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DangKyTaiKhoanActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dang_ky_tai_khoan);
+
+    }
+
+
+}
