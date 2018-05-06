9668c01158c00e79fa81d5fb714f4cacac2f580e
Quoc Bao <quocbao16vn@gmail.com>
2018-05-06 20:53:59 +0700

    comment

--- app/src/main/java/d14cqcp01/group5/carservices/CommentActivity.java
+++ app/src/main/java/d14cqcp01/group5/carservices/CommentActivity.java
@@ -10,6 +10,8 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,9 +36,10 @@ public class CommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ratingbar);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         final String company = "Hoàng Long";    //du lieu ao
-        final String currentUser = "Long zu";   //du lieu ao
+        final String currentUser = user.getDisplayName();
 
         final ArrayList<PhanHoi> phanHoiList = new ArrayList<PhanHoi>();
         final DatabaseReference db = FirebaseDatabase.getInstance().getReference();
